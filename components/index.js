4e310dea2c4de267f1b9a452e19b0dd30b86b96c
Richard Samuelsson <richard.samuelsson@redeye.se>
2018-12-13 17:39:13 +0100

    wip

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Carret } from './src/Carret';
 export { default as If } from './src/If';
 export { default as CTAButton } from './src/CTAButton';
 export { default as RadioButton } from './src/RadioButton';
+export { default as CheckBox } from './src/CheckBox';
---
+++ components/src/CheckBox/index.js
@@ -0,0 +1,110 @@
+/**
+ *
+ * CheckBox
+ *
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.li`
+  list-style: none;
+  text-align: left;
+  margin: 0.75em 0;
+`;
+
+const Text = styled.span`
+  :hover {
+    ${({ disabled }) =>
+    !disabled
+      && `
+      cursor: pointer;
+    `}
+  }
+  color: #ffffff;
+  font-family: Libre Franklin;
+  font-size: 0.875em;
+  text-align: left;
+  font-weight: normal;
+  margin: 1.125em 0;
+  line-height: 1.5;
+  @media (min-width: 786px) {
+    font-size: 1em;
+    color: #e5e5e5;
+  }
+`;
+
+const CheckButton = styled.button`
+  font-size: 1em;
+  width: 1em;
+  height: 1em;
+  box-sizing: border-box;
+  padding: 0;
+  line-height: 1em;
+  margin-right: 0.875em;
+  display: inline-block;
+
+  border: 1px solid #646464;
+
+  color: transparent;
+
+  ${({ checked }) =>
+    checked
+    && `
+    background-image: url(//res.cloudinary.com/redeye/image/upload/v1511866921/check_rzcba8.svg);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: 70% 70%;
+    background-color: #fc6f77;
+    border-color: #fc6f77;
+  `}
+
+  :focus {
+    outline: none;
+  }
+
+  :hover {
+    ${({ disabled }) =>
+    !disabled
+      && `
+      cursor: pointer;
+    `}
+  }
+`;
+
+class CheckListItem extends React.PureComponent {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+    disabled: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    disabled: false,
+  };
+
+  onClick = () => {
+    const { disabled, id, onClick } = this.props;
+    if (!disabled) {
+      onClick(id);
+    }
+  };
+
+  render() {
+    const { checked, disabled, children } = this.props;
+    return (
+      <Wrapper onClick={this.onClick}>
+        <CheckButton
+          checked={checked}
+          disabled={disabled}
+        />
+        <Text disabled={disabled}>{children}</Text>
+      </Wrapper>
+    );
+  }
+}
+
+export default CheckListItem;
