c13ba547038888f5b375ec0bca2049c6167bbc8d
Richard Samuelsson <richard.samuelsson@redeye.se>
2019-02-04 17:22:32 +0100

    wip

--- design/src/Grid/index.js
+++ design/src/Grid/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
+import get from 'lodash/get';
+import invariant from 'invariant';
 
 /* (p/l)(s/m/l)(device) = (portrait/landscape)(small/medium/large)(handset/tablet/computer) */
 const pshandset = 0;
@@ -15,7 +16,6 @@ const lstablet = 960;
 const lltablet = 1024;
 const lscomputer = 1280;
 const lmcomputer = 1360;
-
 
 const minMq = (px) => `@media (min-width: ${px}px)`;
 
@@ -31,7 +31,8 @@ const lltabletMq = minMq(lltablet);
 const lscomputerMq = minMq(lscomputer);
 const lmcomputerMq = minMq(lmcomputer);
 
-const closedMq = (min, max) => `@media (min-width: ${min}px) and (max-width: ${max - 1}px)`;
+const closedMq = (min, max) =>
+  `@media (min-width: ${min}px) and (max-width: ${max - 1}px)`;
 
 const pshandsetClosedMq = closedMq(pshandset, pmhandset);
 const pmhandsetClosedMq = closedMq(pmhandset, plhandset);
@@ -47,49 +48,93 @@ const lmcomputerClosedMq = lmcomputerMq;
 
 const getChildWidth = ({ columns, margin }) => {
   const baseWidth = `calc(${100 / columns}% + ${margin / columns}em`;
-  const width = `${baseWidth} - ${margin * (columns - 1) / columns}em - ${margin * 2 / columns}em)`;
+  const width = `${baseWidth} - ${(margin * (columns - 1))
+    / columns}em - ${(margin * 2) / columns}em)`;
   return width;
 };
 
+
 const getChildCss = ({ width, margin }) => `
-    min-width: ${width};
-    max-width: ${width};
-    margin-bottom: ${margin}em;
-    margin-left: ${margin}em;
+  min-width: ${width};
+  max-width: ${width};
+  margin-bottom: ${margin}em;
+  margin-left: ${margin}em;
+`;
+
+const setChildWidth = (index, css) => `
+    & > *:nth-child(${index + 1}) {
+      ${css};
+    }
   `;
 
-const getSize = ({ margin, columns, children }) => {
+const getSize = ({
+  margin, columns, children, dynamic,
+}) => {
   const count = React.Children.count(children);
-  /* cc = calculatedColumns */
-  let nthChildRowException = null;
-  let remainder = count % columns;
-  if (count < columns) {
-    nthChildRowException = 0;
-  } else if (count > columns && remainder !== 0) {
-    nthChildRowException = count - remainder + 1;
-  }
 
-  if (remainder % 2 === 1 && count !== 1) {
+  const spans = [children].flat().map(({ props }) => {
+    const propSpan = get(props, 'span');
+    if (propSpan) {
+      const number = Number(propSpan);
+      invariant(
+        typeof number === 'number' && !Number.isNaN(number),
+        'number props must be valid number!',
+      );
+      return number;
+    }
+    return null;
+  });
+
+  /* start lastRowCalulations: make the last row span correctly */
+  const countWithSpans = count + spans.reduce((total, span) => (span ? span - 1 : 0), 0);
+  let nthChildRowException = null;
+  let remainder = countWithSpans % columns;
+  if (countWithSpans < columns) {
+    nthChildRowException = 0;
+  } else if (countWithSpans > columns && remainder !== 0) {
+    nthChildRowException = countWithSpans - remainder + 1;
+  }
+  if (remainder % 2 === 1 && countWithSpans !== 1) {
     remainder += 1;
   }
-
-  const width = getChildWidth({ columns, margin });
+  /* end lastRowCalulations */
 
   let css = `
     padding-top: ${margin}em;
     padding-right: ${margin}em;
-    & > * {
-      ${getChildCss({ width, margin })};
-    }
   `;
-  if (nthChildRowException !== null) {
-    const lastRowWidth = getChildWidth({ columns: remainder, margin });
-    css += `
-      & > *:nth-child(n+${nthChildRowException}) {
-        ${getChildCss({ width: lastRowWidth, margin })};
+
+  css += spans
+    .map((span, index) => {
+      let calculatedColumns = columns;
+
+      if (
+        dynamic
+        && nthChildRowException !== null
+        && index >= nthChildRowException
+      ) {
+        calculatedColumns = remainder;
       }
-    `;
-  }
+
+      if (span !== null) {
+        calculatedColumns = 12 / span;
+      }
+
+      const width = getChildWidth({ columns: calculatedColumns, margin });
+
+      return setChildWidth(index, getChildCss({ width, margin }));
+    })
+    .join('\n');
+
+  // if (nthChildRowException !== null) {
+  //   const lastRowWidth = getChildWidth({ columns: remainder, margin });
+  //   css += `
+  //     & > *:nth-child(n+${nthChildRowException}) {
+  //       ${getChildCss({ width: lastRowWidth, margin })};
+  //     }
+  //   `;
+  // }
+
   return css;
 };
 const Grid = styled.div`
@@ -98,24 +143,41 @@ const Grid = styled.div`
   justify-content: flex-start;
   flex-wrap: wrap;
   ${closedMq(0, pstablet)} {
-    ${({ children }) => getSize({ margin: 1, columns: 4, children })};
+    ${({ children, dynamic }) =>
+    getSize({
+      margin: 1, columns: 4, children, dynamic,
+    })};
   }
   ${closedMq(pstablet, pltablet)} {
-    ${({ children }) => getSize({ margin: 1, columns: 8, children })};
+    ${({ children, dynamic }) =>
+    getSize({
+      margin: 1, columns: 8, children, dynamic,
+    })};
   }
   ${closedMq(pltablet, lstablet)} {
-    ${({ children }) => getSize({ margin: 1.5, columns: 8, children })};
+    ${({ children, dynamic }) =>
+    getSize({
+      margin: 1.5, columns: 8, children, dynamic,
+    })};
   }
   ${closedMq(lstablet, lltablet)} {
-    ${({ children }) => getSize({ margin: 1.5, columns: 12, children })};
+    ${({ children, dynamic }) =>
+    getSize({
+      margin: 1.5, columns: 12, children, dynamic,
+    })};
   }
   ${closedMq(lltablet, lmcomputer)} {
-    ${({ children }) => getSize({ margin: 2, columns: 12, children })};
+    ${({ children, dynamic }) =>
+    getSize({
+      margin: 2, columns: 12, children, dynamic,
+    })};
   }
   ${lmcomputerMq} {
-    ${({ children }) => getSize({ margin: 2.5, columns: 12, children })};
+    ${({ children, dynamic }) =>
+    getSize({
+      margin: 2.5, columns: 12, children, dynamic,
+    })};
   }
 `;
-
 
 export default Grid;
--- design/src/Grid/index.story.js
+++ design/src/Grid/index.story.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Grid from '.';
 
-
 const range = (l) => {
   const numbers = [];
   for (let i = 1; i <= l; i += 1) {
@@ -12,8 +11,6 @@ const range = (l) => {
   }
   return numbers;
 };
-
-
 
 const Box = styled.div`
   background-color: red;
@@ -27,17 +24,279 @@ const Container = styled.div`
   margin: 2em 0;
 `;
 
-
 storiesOf('Grid', module)
-  .add('Normal', () => (
+  .add('With dynamic column width', () => (
     <>
       {range(12).map((i) => (
         <Container key={i}>
           <p>{i}</p>
-          <Grid>
-            {range(i).map((ii) => <Box key={ii} />)}
+          <Grid dynamic>
+            {range(i).map((ii) => (
+              <Box key={ii} />
+            ))}
           </Grid>
         </Container>
       ))}
+    </>
+  ))
+  .add('With specific with', () => (
+    <>
+      {/* <Container>
+        <p>Span 1/12</p>
+        <Grid>
+          <Box span="1" />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 2/12</p>
+        <Grid>
+          <Box span="2" />
+        </Grid>
+      </Container> */}
+      <Container>
+        <p>Span 1/12</p>
+        <Grid>
+          <Box span="1" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 2/12</p>
+        <Grid>
+          <Box span="2" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 3/12</p>
+        <Grid>
+          <Box span="3" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 4/12</p>
+        <Grid>
+          <Box span="4" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 5/12</p>
+        <Grid>
+          <Box span="5" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 6/12</p>
+        <Grid>
+          <Box span="6" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 7/12</p>
+        <Grid>
+          <Box span="7" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 8/12</p>
+        <Grid>
+          <Box span="8" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 9/12</p>
+        <Grid>
+          <Box span="9" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 10/12</p>
+        <Grid>
+          <Box span="10" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 11/12</p>
+        <Grid>
+          <Box span="11" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span 12/12</p>
+        <Grid>
+          <Box span="12" />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
+      <Container>
+        <p>Span in middle</p>
+        <Grid>
+          <Box />
+          <Box span={8} />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+      </Container>
     </>
   ));
