ca736d9be1a260e36c1d435d66c8073e9d6972dc
Richard Samuelsson <richard.samuelsson@redeye.se>
2019-02-13 17:40:02 +0100

    feat(design,components): ci wip

--- ci/publish.js
+++ ci/publish.js
@@ -30,7 +30,7 @@ execa('git', ['log', '-1', '--pretty=%B'])
         return [
           // `npm set registry=https://registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}`,
           `cd ${cwd}`,
-          'yarn publish --access public',
+          'npm publish --access public',
         ].join('\n');
       })
       .join('\n')}\n`;
