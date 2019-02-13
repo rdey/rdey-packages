f02ae1f1d45234ba13cea073a5719a7912096e21
Richard Samuelsson <richard.samuelsson@redeye.se>
2019-02-13 18:07:00 +0100

    feat(design): update api

--- design/src/Text/index.js
+++ design/src/Text/index.js
@@ -28,7 +28,7 @@ export const inter = `
 
 export const styles = [
   {
-    name: 'display56M',
+    name: 'Display56M',
     id: 'display-56-m',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -46,7 +46,7 @@ export const styles = [
     },
   },
   {
-    name: 'display56MU',
+    name: 'Display56MU',
     id: 'display-56-m-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -66,7 +66,7 @@ export const styles = [
     },
   },
   {
-    name: 'display56R',
+    name: 'Display56R',
     id: 'display-56-r',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -84,7 +84,7 @@ export const styles = [
     },
   },
   {
-    name: 'display56RU',
+    name: 'Display56RU',
     id: 'display-56-r-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -105,7 +105,7 @@ export const styles = [
   },
 
   {
-    name: 'display56L',
+    name: 'Display56L',
     id: 'display-56-l',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -123,7 +123,7 @@ export const styles = [
     },
   },
   {
-    name: 'display56LU',
+    name: 'Display56LU',
     id: 'display-56-l-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -143,7 +143,7 @@ export const styles = [
     },
   },
   {
-    name: 'display40M',
+    name: 'Display40M',
     id: 'display-40-m',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -161,7 +161,7 @@ export const styles = [
     },
   },
   {
-    name: 'display40MU',
+    name: 'Display40MU',
     id: 'display-40-m-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -181,7 +181,7 @@ export const styles = [
     },
   },
   {
-    name: 'display40R',
+    name: 'Display40R',
     id: 'display-40-r',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -199,7 +199,7 @@ export const styles = [
     },
   },
   {
-    name: 'display40RU',
+    name: 'Display40RU',
     id: 'display-40-r-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -219,7 +219,7 @@ export const styles = [
     },
   },
   {
-    name: 'display40LU',
+    name: 'Display40LU',
     id: 'display-40-l-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -239,7 +239,7 @@ export const styles = [
     },
   },
   {
-    name: 'titlesDisplay40L',
+    name: 'TitlesDisplay40L',
     id: 'titles/display-40-l',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -257,7 +257,7 @@ export const styles = [
     },
   },
   {
-    name: 'header32M',
+    name: 'Header32M',
     id: 'header-32-m',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -275,7 +275,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader32MU',
+    name: 'HeadersUppercaseHeader32MU',
     id: 'headers/uppercase/header-32-m-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -295,7 +295,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader32R',
+    name: 'HeadersHeader32R',
     id: 'headers/header-32-r',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -313,7 +313,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader32RU',
+    name: 'HeadersUppercaseHeader32RU',
     id: 'headers/uppercase/header-32-r-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -333,7 +333,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader32L',
+    name: 'HeadersHeader32L',
     id: 'headers/header-32-l',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -351,7 +351,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader32LU',
+    name: 'HeadersUppercaseHeader32LU',
     id: 'headers/uppercase/header-32-l-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -371,7 +371,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader28M',
+    name: 'HeadersHeader28M',
     id: 'headers/header-28-m',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -389,7 +389,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader28MU',
+    name: 'HeadersUppercaseHeader28MU',
     id: 'headers/uppercase/header-28-m-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -409,7 +409,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader28R',
+    name: 'HeadersHeader28R',
     id: 'headers/header-28-r',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -427,7 +427,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader28RU',
+    name: 'HeadersUppercaseHeader28RU',
     id: 'headers/uppercase/header-28-r-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -447,7 +447,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader28L',
+    name: 'HeadersHeader28L',
     id: 'headers/header-28-l',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -465,7 +465,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader28LU',
+    name: 'HeadersUppercaseHeader28LU',
     id: 'headers/uppercase/header-28-l-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -485,7 +485,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader24M',
+    name: 'HeadersHeader24M',
     id: 'headers/header-24-m',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -503,7 +503,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader24MU',
+    name: 'HeadersUppercaseHeader24MU',
     id: 'headers/uppercase/header-24-m-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -523,7 +523,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader24R',
+    name: 'HeadersHeader24R',
     id: 'headers/header-24-r',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -541,7 +541,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader24RU',
+    name: 'HeadersUppercaseHeader24RU',
     id: 'headers/uppercase/header-24-r-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -561,7 +561,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersHeader24L',
+    name: 'HeadersHeader24L',
     id: 'headers/header-24-l',
     sc: (tag) => styled[tag]`
       font-family: Oswald;
@@ -579,7 +579,7 @@ export const styles = [
     },
   },
   {
-    name: 'headersUppercaseHeader24LU',
+    name: 'HeadersUppercaseHeader24LU',
     id: 'headers/uppercase/header-24-l-u',
     sc: (tag) => styled[tag]`
       text-transform: uppercase;
@@ -599,7 +599,7 @@ export const styles = [
     },
   },
   {
-    name: 'bodyBody20',
+    name: 'BodyBody20',
     id: 'body/body-20',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -613,10 +613,11 @@ export const styles = [
       fontSize: '20px',
       lineHeight: '24px',
       letterSpacing: '0.15px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'subtitlesSubtitle16',
+    name: 'SubtitlesSubtitle16',
     id: 'subtitles/subtitle-16',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -630,10 +631,11 @@ export const styles = [
       fontSize: '16px',
       lineHeight: '20px',
       letterSpacing: '0.5px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'bodyBody16',
+    name: 'BodyBody16',
     id: 'body/body-16',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -647,10 +649,11 @@ export const styles = [
       fontSize: '16px',
       lineHeight: '24px',
       letterSpacing: '0.5px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'otherButton14',
+    name: 'OtherButton14',
     id: 'other/button-14',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -664,10 +667,11 @@ export const styles = [
       fontSize: '14px',
       lineHeight: '14px',
       letterSpacing: '0.75px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'subtitlesSubtitle14',
+    name: 'SubtitlesSubtitle14',
     id: 'subtitles/subtitle-14',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -681,10 +685,11 @@ export const styles = [
       fontSize: '14px',
       lineHeight: '18px',
       letterSpacing: '0.5px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'bodyBody14',
+    name: 'BodyBody14',
     id: 'body/body-14',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -698,10 +703,11 @@ export const styles = [
       fontSize: '14px',
       lineHeight: '22px',
       letterSpacing: '0.25px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'otherButton12',
+    name: 'OtherButton12',
     id: 'other/button-12',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -715,10 +721,11 @@ export const styles = [
       fontSize: '12px',
       lineHeight: '12px',
       letterSpacing: '1px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'otherCaption12',
+    name: 'OtherCaption12',
     id: 'other/caption-12',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -732,10 +739,11 @@ export const styles = [
       fontSize: '12px',
       lineHeight: '16px',
       letterSpacing: '0.4px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'otherOverline10',
+    name: 'OtherOverline10',
     id: 'other/overline-10',
     sc: (tag) => styled[tag]`
       ${inter};
@@ -749,67 +757,68 @@ export const styles = [
       fontSize: '10px',
       lineHeight: '10px',
       letterSpacing: '0.5px',
+      fontFamily: 'Inter',
     },
   },
   {
-    name: 'otherTabular09',
+    name: 'OtherTabular09',
     id: 'other/tabular-09',
     sc: (tag) => styled[tag]`
-      font-family: ${inter};
+      ${inter};
       font-weight: ${regular};
       font-size: 9px;
       line-height: 9px;
       letter-spacing: 1px;
     `,
     css: {
-      fontFamily: `${inter}`,
       fontWeight: `${regular}`,
       fontSize: '9px',
       lineHeight: '9px',
       letterSpacing: '1px',
+      fontFamily: 'Inter',
     },
   },
 ];
 
 const styleDict = fromPairs(styles.map((p) => [p.id, p]));
 
-export const display56M = styleDict['display-56-m'];
-export const display56MU = styleDict['display-56-m-u'];
-export const display56R = styleDict['display-56-r'];
-export const display56RU = styleDict['display-56-r-u'];
-export const display56L = styleDict['display-56-l'];
-export const display56LU = styleDict['display-56-l-u'];
-export const display40M = styleDict['display-40-m'];
-export const display40MU = styleDict['display-40-m-u'];
-export const display40R = styleDict['display-40-r'];
-export const display40RU = styleDict['display-40-r-u'];
-export const display40LU = styleDict['display-40-l-u'];
-export const titlesDisplay40L = styleDict['titles/display-40-l'];
-export const header32M = styleDict['header-32-m'];
-export const headersUppercaseHeader32MU = styleDict['headers/uppercase/header-32-m-u'];
-export const headersHeader32R = styleDict['headers/header-32-r'];
-export const headersUppercaseHeader32RU = styleDict['headers/uppercase/header-32-r-u'];
-export const headersHeader32L = styleDict['headers/header-32-l'];
-export const headersUppercaseHeader32LU = styleDict['headers/uppercase/header-32-l-u'];
-export const headersHeader28M = styleDict['headers/header-28-m'];
-export const headersUppercaseHeader28MU = styleDict['headers/uppercase/header-28-m-u'];
-export const headersHeader28R = styleDict['headers/header-28-r'];
-export const headersUppercaseHeader28RU = styleDict['headers/uppercase/header-28-r-u'];
-export const headersHeader28L = styleDict['headers/header-28-l'];
-export const headersUppercaseHeader28LU = styleDict['headers/uppercase/header-28-l-u'];
-export const headersHeader24M = styleDict['headers/header-24-m'];
-export const headersUppercaseHeader24MU = styleDict['headers/uppercase/header-24-m-u'];
-export const headersHeader24R = styleDict['headers/header-24-r'];
-export const headersUppercaseHeader24RU = styleDict['headers/uppercase/header-24-r-u'];
-export const headersHeader24L = styleDict['headers/header-24-l'];
-export const headersUppercaseHeader24LU = styleDict['headers/uppercase/header-24-l-u'];
-export const bodyBody20 = styleDict['body/body-20'];
-export const subtitlesSubtitle16 = styleDict['subtitles/subtitle-16'];
-export const bodyBody16 = styleDict['body/body-16'];
-export const otherButton14 = styleDict['other/button-14'];
-export const subtitlesSubtitle14 = styleDict['subtitles/subtitle-14'];
-export const bodyBody14 = styleDict['body/body-14'];
-export const otherButton12 = styleDict['other/button-12'];
-export const otherCaption12 = styleDict['other/caption-12'];
-export const otherOverline10 = styleDict['other/overline-10'];
-export const otherTabular09 = styleDict['other/tabular-09'];
+export const Display56M = styleDict['display-56-m'];
+export const Display56MU = styleDict['display-56-m-u'];
+export const Display56R = styleDict['display-56-r'];
+export const Display56RU = styleDict['display-56-r-u'];
+export const Display56L = styleDict['display-56-l'];
+export const Display56LU = styleDict['display-56-l-u'];
+export const Display40M = styleDict['display-40-m'];
+export const Display40MU = styleDict['display-40-m-u'];
+export const Display40R = styleDict['display-40-r'];
+export const Display40RU = styleDict['display-40-r-u'];
+export const Display40LU = styleDict['display-40-l-u'];
+export const TitlesDisplay40L = styleDict['titles/display-40-l'];
+export const Header32M = styleDict['header-32-m'];
+export const HeadersUppercaseHeader32MU = styleDict['headers/uppercase/header-32-m-u'];
+export const HeadersHeader32R = styleDict['headers/header-32-r'];
+export const HeadersUppercaseHeader32RU = styleDict['headers/uppercase/header-32-r-u'];
+export const HeadersHeader32L = styleDict['headers/header-32-l'];
+export const HeadersUppercaseHeader32LU = styleDict['headers/uppercase/header-32-l-u'];
+export const HeadersHeader28M = styleDict['headers/header-28-m'];
+export const HeadersUppercaseHeader28MU = styleDict['headers/uppercase/header-28-m-u'];
+export const HeadersHeader28R = styleDict['headers/header-28-r'];
+export const HeadersUppercaseHeader28RU = styleDict['headers/uppercase/header-28-r-u'];
+export const HeadersHeader28L = styleDict['headers/header-28-l'];
+export const HeadersUppercaseHeader28LU = styleDict['headers/uppercase/header-28-l-u'];
+export const HeadersHeader24M = styleDict['headers/header-24-m'];
+export const HeadersUppercaseHeader24MU = styleDict['headers/uppercase/header-24-m-u'];
+export const HeadersHeader24R = styleDict['headers/header-24-r'];
+export const HeadersUppercaseHeader24RU = styleDict['headers/uppercase/header-24-r-u'];
+export const HeadersHeader24L = styleDict['headers/header-24-l'];
+export const HeadersUppercaseHeader24LU = styleDict['headers/uppercase/header-24-l-u'];
+export const BodyBody20 = styleDict['body/body-20'];
+export const SubtitlesSubtitle16 = styleDict['subtitles/subtitle-16'];
+export const BodyBody16 = styleDict['body/body-16'];
+export const OtherButton14 = styleDict['other/button-14'];
+export const SubtitlesSubtitle14 = styleDict['subtitles/subtitle-14'];
+export const BodyBody14 = styleDict['body/body-14'];
+export const OtherButton12 = styleDict['other/button-12'];
+export const OtherCaption12 = styleDict['other/caption-12'];
+export const OtherOverline10 = styleDict['other/overline-10'];
+export const OtherTabular09 = styleDict['other/tabular-09'];
--- design/src/Text/index.story.js
+++ design/src/Text/index.story.js
@@ -1,50 +1,60 @@
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { styles } from '.';
+import React from 'react';
+import styled from 'styled-components';
+import { Display56MU, styles } from '.';
+
+const Wrapper = styled.div`
+  padding: 1em;
+`;
 
 const Blocks = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: stretch;
+  display: grid;
+  grid-template-columns: 1em auto 1em auto 1em;
+  grid-template-rows: auto;
 `;
 
-const Block = styled.div`
-  background: ${({ color }) => color};
-  padding: 1em;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex: 1;
-`;
-
-const Text = styled.div`
-  color: black;
+const Pre = styled.pre`
+  padding-left: 1em;
+  border-left: 1px solid black;
+  grid-column-start: 4;
 `;
 
 storiesOf('Design/Text', module).add('Fonts', () => (
   <>
-    <h1>Fonts</h1>
-    <p>
-      <a href="https://app.zeplin.io/project/5c01378f7608892c84851d79/screen/5c0147744129e801cdf905ed">
-        Source
-      </a>
-    </p>
-    <p>
-      <a href="https://github.com/rdey/designsystem/wiki/04.-Typography">
-        Read more
-      </a>
-    </p>
+    <Wrapper>
+      <h1 style={Display56MU.css}>Fonts</h1>
+      <p>
+        <a href="https://app.zeplin.io/project/5c01378f7608892c84851d79/screen/5c0147744129e801cdf905ed">
+          Source
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/rdey/designsystem/wiki/04.-Typography">
+          Read more
+        </a>
+      </p>
+    </Wrapper>
     <Blocks>
-      {styles.map(({ name, id, css }) => {
-        const Font = styled(css('div'))`
+      {styles.map(({
+        name, id, css, sc,
+      }) => {
+        const Font = styled(sc('div'))`
           white-space: nowrap;
+          grid-column-start: 2;
         `;
         return (
-          <Block key={name}>
+          <React.Fragment key={name}>
             <Font>{id}</Font>
-          </Block>
+            <Pre>
+              <>
+                {name}
+                {' '}
+                <>=</>
+                {' '}
+              </>
+              {JSON.stringify(css, null, 2)}
+            </Pre>
+          </React.Fragment>
         );
       })}
     </Blocks>
