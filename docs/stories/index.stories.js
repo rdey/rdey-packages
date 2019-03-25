b389944765b7218237db20729b53ebe787aba81d
Richard Samuelsson <richard.samuelsson@redeye.se>
2019-03-25 18:25:15 +0100

    wip

--- docs/stories/index.stories.js
+++ docs/stories/index.stories.js
@@ -10,8 +10,6 @@ import '../../components/src/icons/Close/index.story';
 import '../../components/src/LoadingSpinner/index.story';
 import '../../components/src/Tabs/index.story';
 
-import '../../design/src/Colors/index.story';
-
 import '../../grid/index.story';
 
 const RandomCompMainHeader = styled.h1`
