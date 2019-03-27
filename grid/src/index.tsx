e85a6720030e57596b6c9b33acfbf83beb7b88c7
Richard Samuelsson <richard.samuelsson@redeye.se>
2019-03-27 15:43:05 +0100

    wip on grid

--- grid/src/index.tsx
+++ grid/src/index.tsx
@@ -7,24 +7,66 @@ import styled, { ThemeProvider } from 'styled-components';
 import { getViewport, numberOfCols, margins } from '@rdey/design';
 // import console = require('console');
 
+type Position = {
+  x: number;
+  y: number;
+  z: number;
+};
+type Positions = {
+  [key: string]: Position;
+};
+type State = {
+  hash: null | string;
+  positions: Positions;
+  animate: boolean;
+};
+
+type Items = Array<{ key: string; Cell: React.FC }>;
+
+type Props = {
+  items: Items;
+};
+
+type Action = {
+  type: 'SET_ITEM' | 'SET_HASH' | 'STOP_ANIMATION';
+  hash?: string;
+  key?: string;
+  el?: HTMLElement | null;
+};
+
+const OuterBound = styled.div`
+  position: relative;
+`;
+
 const Wrapper = styled.div``;
 
-const Row =
-  styled.div <
-  { emptySpace: number } >
-  `
+const AbsoluteWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  visibility: hidden;
+`;
+
+const Row = styled.div<{ emptySpace: number }>`
   display: flex;
   justify-content: flex-start;
 `;
 
-type Props = {
-  items: Array<{ key: string, Cell: React.FC }>,
-};
+const TRANSITION_DURATION: number = 5000;
 
-const CellWrapper =
-  styled.div <
-  { first: boolean, last: boolean } >
-  `
+const CellWrapper = styled.div<
+  { first: boolean; last: boolean; shouldHaveTransition: boolean } & Position
+>`
+
+  ${({ shouldHaveTransition }) =>
+    shouldHaveTransition &&
+    `
+    transition: transform ${TRANSITION_DURATION}ms ease;
+  `}
+
+
   ${({ theme: { viewport }, first, last }) => {
     const columns = numberOfCols[viewport];
     const margin = margins[viewport];
@@ -37,6 +79,9 @@ const CellWrapper =
       ${first ? `margin-left: ${margin}px` : ''}
       ${!last ? `margin-right: ${margin}px` : ''}
     `;
+  }}
+  ${({ x, y, z }) => {
+    return `transform: translate3d(${[x, y, z].join('px,')}px);`;
   }}
 `;
 
@@ -55,42 +100,216 @@ function getRows<T>(arr: T[], rowSize: number): T[][] {
   return rows;
 }
 
-export default class Grid extends React.Component<Props> {
-  registerChild = (el: HTMLElement | null) => {
-    if (!el) {
-      return;
-    }
-    const top = el.offsetTop;
-    const left = el.offsetLeft;
-    console.log(top, left);
+function init(items: Items): State {
+  const positions = items.reduce(
+    (c, { key }) => {
+      return {
+        ...c,
+        [key]: {
+          x: 0,
+          y: 0,
+          z: -100,
+        },
+      };
+    },
+    {} as { [key: string]: Position }
+  );
+  return {
+    hash: null,
+    positions,
+    animate: false,
   };
-  render() {
-    const { items } = this.props;
-    const viewport = getViewport();
-    const cols = numberOfCols[viewport];
-    return (
-      <ThemeProvider theme={{ viewport }}>
-        <Wrapper>
-          {getRows(items, cols).map((rowItems, index) => {
-            return (
-              <Row emptySpace={cols - rowItems.length} key={index}>
-                {rowItems.map((Item, index) => {
-                  return (
-                    <CellWrapper
-                      key={Item.key}
-                      first={index === 0}
-                      last={index === rowItems.length - 1}
-                      ref={this.registerChild}
-                    >
-                      <Item.Cell />
-                    </CellWrapper>
-                  );
-                })}
-              </Row>
-            );
-          })}
-        </Wrapper>
-      </ThemeProvider>
-    );
+}
+
+const setItem = (state: State, action: Action) => {
+  const { el, key } = action;
+  if (!key || !el) {
+    throw new Error();
+  }
+  const y = el.offsetTop;
+  const x = el.offsetLeft;
+  return {
+    ...state,
+    positions: {
+      ...state.positions,
+      [key]: {
+        x,
+        y,
+        z: 1,
+      },
+    },
+  };
+};
+
+const getItemsHash = (items: Items) => {
+  return items.map(({ key }) => key).join(',');
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'SET_ITEM':
+      return setItem(state, action);
+    case 'SET_HASH':
+      if (!action.hash) {
+        throw new Error();
+      }
+      return {
+        ...state,
+        hash: action.hash,
+        animate: true,
+      };
+    case 'STOP_ANIMATION':
+      return {
+        ...state,
+        animate: false,
+      };
+    default:
+      throw new Error();
   }
 }
+
+const Grid = ({ items }: Props) => {
+  const viewport = getViewport();
+  const cols = numberOfCols[viewport];
+  const [{ positions, hash, animate }, dispatch] = React.useReducer(
+    reducer,
+    items,
+    init
+  );
+  const refs = React.useRef<{
+    currentPositions: Positions;
+    nextPositions: Positions;
+    currentItems: Items;
+    previousItems: Items;
+  }>({
+    currentPositions: {},
+    nextPositions: {},
+    currentItems: items,
+    previousItems: [],
+  });
+
+  const newHash = getItemsHash(items);
+
+  const newGrid = newHash !== hash;
+
+  if (newGrid) {
+    if (!animate) {
+      refs.current.previousItems = refs.current.currentItems;
+    }
+    refs.current.currentItems = items;
+  }
+
+  React.useEffect(() => {
+    dispatch({
+      type: 'SET_HASH',
+      hash: newHash,
+    });
+  }, [newHash]);
+
+  React.useEffect(() => {
+    let timer: number;
+    const clear = () => {
+      window.clearTimeout(timer);
+    };
+    if (animate) {
+      timer = window.setTimeout(() => {
+        dispatch({
+          type: 'STOP_ANIMATION',
+        });
+      }, TRANSITION_DURATION);
+    } else {
+      clear();
+    }
+    return clear;
+  }, [animate, newHash]);
+
+  const rows = (
+    itemsToRender: Items,
+    ref: (key: string, el: HTMLElement | null) => void,
+    translateMatrix = positions
+  ) =>
+    getRows(itemsToRender, cols).map((rowItems, index) => {
+      return (
+        <Row emptySpace={cols - rowItems.length} key={index}>
+          {rowItems.map(({ Cell, key }, index) => {
+            return (
+              <CellWrapper
+                key={key}
+                first={index === 0}
+                last={index === rowItems.length - 1}
+                {...translateMatrix[key]}
+                ref={(el) => ref(key, el)}
+                shouldHaveTransition={animate}
+              >
+                <Cell />
+              </CellWrapper>
+            );
+          })}
+        </Row>
+      );
+    });
+
+  let renderItems = refs.current.currentItems;
+  let measureItems = refs.current.currentItems;
+  if (newGrid || animate) {
+    renderItems = refs.current.previousItems;
+    measureItems = refs.current.currentItems;
+  }
+
+  let translateMatrix = positions;
+
+  if (animate) {
+    translateMatrix = measureItems.reduce(
+      (c, { key }) => {
+        const { x: bx, y: by, z: bz } = refs.current.nextPositions[key];
+        const { x: ax, y: ay, z: az } = refs.current.currentPositions[key];
+        return {
+          ...c,
+          [key]: {
+            x: bx - ax,
+            y: by - ay,
+            z: bz - az,
+          },
+        };
+      },
+      {} as Positions
+    );
+  }
+
+  return (
+    <ThemeProvider theme={{ viewport }}>
+      <OuterBound>
+        <Wrapper>
+          {rows(
+            renderItems,
+            (key, el) => {
+              if (el) {
+                refs.current.currentPositions[key] = {
+                  x: el.offsetLeft,
+                  y: el.offsetTop,
+                  z: 0,
+                };
+              }
+            },
+            translateMatrix
+          )}
+        </Wrapper>
+        {newGrid && (
+          <AbsoluteWrapper>
+            {rows(measureItems, (key, el) => {
+              if (el) {
+                refs.current.nextPositions[key] = {
+                  x: el.offsetLeft,
+                  y: el.offsetTop,
+                  z: 0,
+                };
+              }
+            })}
+          </AbsoluteWrapper>
+        )}
+      </OuterBound>
+    </ThemeProvider>
+  );
+};
+
+export default Grid;
